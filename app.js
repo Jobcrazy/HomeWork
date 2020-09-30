b84943e0a1ea483ae5f542bec6f04e87a0838397
LiuHang <liuhang@kaka888.com>
2020-09-30 00:23:21 -0700

    Bug fix
    Signed-off-by: LiuHang <liuhang@kaka888.com>

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('trust proxy', 1) // trust first proxy
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(process.env.DATABASE || path.join(__dirname, 'public')));
+app.use(express.static(process.env.PUBLIC || path.join(__dirname, 'public')));
 
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");

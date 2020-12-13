6a672d6b521925b79e17400b7829dd07c48d82af
LiuHang <liuhang@kaka888.com>
2020-12-12 19:52:01 -0800

    Update Database
    Signed-off-by: LiuHang <liuhang@kaka888.com>

--- database/homework_database.sql
+++ database/homework_database.sql
@@ -1,60 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- 主机： localhost
--- 生成日期： 2020-09-28 10:37:40
--- 服务器版本： 5.7.26
--- PHP 版本： 7.3.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- 数据库： `homework_database`
---
-
--- --------------------------------------------------------
-
---
--- 表的结构 `hw_admin`
---
 
 CREATE TABLE `hw_admin` (
   `id` int(11) NOT NULL COMMENT 'ID',
   `uid` int(11) NOT NULL COMMENT 'User ID in table hw_user'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `hw_course`
---
-
 CREATE TABLE `hw_course` (
   `id` int(11) NOT NULL COMMENT 'Course ID',
   `innercourseid` varchar(80) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Official Course ID with no space',
   `courseid` varchar(80) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Official Course ID',
+  `name` varchar(250) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Course Name',
   `school` varchar(80) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'British Columbia Institution of Technology' COMMENT 'School Name (Default:BCIT)',
   `term` varchar(80) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Term',
   `instructor` varchar(80) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Instructor Name',
   `class` varchar(80) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Class/Group/SET',
   `logo` varchar(4096) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Course Logp'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `hw_done`
---
 
 CREATE TABLE `hw_done` (
   `id` int(10) UNSIGNED NOT NULL COMMENT 'ID',
@@ -63,23 +33,11 @@ CREATE TABLE `hw_done` (
   `kid` int(10) UNSIGNED NOT NULL COMMENT 'Homewrok ID'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `hw_follow`
---
-
 CREATE TABLE `hw_follow` (
   `id` int(10) UNSIGNED NOT NULL COMMENT 'ID',
   `uid` int(10) UNSIGNED NOT NULL COMMENT 'User ID',
   `cid` int(10) UNSIGNED NOT NULL COMMENT 'Inner Course ID'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `hw_homework`
---
 
 CREATE TABLE `hw_homework` (
   `id` int(10) UNSIGNED NOT NULL COMMENT 'ID',
@@ -88,12 +46,6 @@ CREATE TABLE `hw_homework` (
   `description` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'Details',
   `due` datetime DEFAULT NULL COMMENT 'Due Date Time'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `hw_user`
---
 
 CREATE TABLE `hw_user` (
   `id` int(10) UNSIGNED NOT NULL COMMENT 'User ID',
@@ -106,118 +58,65 @@ CREATE TABLE `hw_user` (
   `token` varchar(40) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Token'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `hw_volunteer`
---
-
 CREATE TABLE `hw_volunteer` (
   `id` int(10) UNSIGNED NOT NULL COMMENT 'ID',
   `cid` int(10) UNSIGNED NOT NULL COMMENT 'Course Index ID',
   `vid` int(10) UNSIGNED NOT NULL COMMENT 'User ID'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- 转储表的索引
---
 
---
--- 表的索引 `hw_admin`
---
 ALTER TABLE `hw_admin`
   ADD PRIMARY KEY (`id`),
   ADD KEY `uid` (`uid`);
 
---
--- 表的索引 `hw_course`
---
 ALTER TABLE `hw_course`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `name` (`name`);
 
---
--- 表的索引 `hw_done`
---
 ALTER TABLE `hw_done`
   ADD PRIMARY KEY (`id`),
   ADD KEY `uid` (`uid`),
   ADD KEY `kid` (`kid`),
   ADD KEY `cid` (`cid`);
 
---
--- 表的索引 `hw_follow`
---
 ALTER TABLE `hw_follow`
   ADD PRIMARY KEY (`id`),
   ADD KEY `uid` (`uid`);
 
---
--- 表的索引 `hw_homework`
---
 ALTER TABLE `hw_homework`
   ADD PRIMARY KEY (`id`),
   ADD KEY `cid` (`cid`),
   ADD KEY `due` (`due`);
 
---
--- 表的索引 `hw_user`
---
 ALTER TABLE `hw_user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `gid` (`gid`) USING BTREE,
   ADD KEY `email` (`email`);
 
---
--- 表的索引 `hw_volunteer`
---
 ALTER TABLE `hw_volunteer`
   ADD PRIMARY KEY (`id`),
   ADD KEY `cid` (`cid`),
   ADD KEY `uid` (`vid`);
 
---
--- 在导出的表使用AUTO_INCREMENT
---
 
---
--- 使用表AUTO_INCREMENT `hw_admin`
---
 ALTER TABLE `hw_admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID';
 
---
--- 使用表AUTO_INCREMENT `hw_course`
---
 ALTER TABLE `hw_course`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Course ID';
 
---
--- 使用表AUTO_INCREMENT `hw_done`
---
 ALTER TABLE `hw_done`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID';
 
---
--- 使用表AUTO_INCREMENT `hw_follow`
---
 ALTER TABLE `hw_follow`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID';
 
---
--- 使用表AUTO_INCREMENT `hw_homework`
---
 ALTER TABLE `hw_homework`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID';
 
---
--- 使用表AUTO_INCREMENT `hw_user`
---
 ALTER TABLE `hw_user`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'User ID';
 
---
--- 使用表AUTO_INCREMENT `hw_volunteer`
---
 ALTER TABLE `hw_volunteer`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID';
 COMMIT;
